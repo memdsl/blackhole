62c3f4226a33c7c48c44788aced9fe7429e24e9c
myyerrol <myyerrol@126.com>
2024-10-03 09:00:00 +0800

    feat: add new headers

--- include/common.hpp
+++ include/common.hpp
@@ -1,16 +1,36 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <fmt/core.h>
+#include <fmt/color.h>
+#include <fmt/ranges.h>
+
+#include <CLI11.hpp>
+
 #include <autoconf.h>
-#include <util/time.hpp>
+#include <time.hpp>
+
+using namespace std;
 
 #define CONCAT(x, y) x ## y
 
 #define MUX_DEF_0 X,
 #define MUX_DEF_1 X,
-#define MUX_CHOOSE_2ND(a, b, ...) b
-#define MUX_JUDGE_COMMA(mux_comma, a, b) MUX_CHOOSE_2ND(mux_comma a, b)
-#define MUX_JUDGE(mux_def, macro, a, b) MUX_JUDGE_COMMA(CONCAT(mux_def, macro), a, b)
-#define MUX(macro, x, y)  MUX_JUDGE(MUX_DEF_, macro, x, y)
+#define MUX_DEF_CHOOSE_2ND(a, b, ...) b
+#define MUX_DEF_JUDGE_COMMA(mux_comma, a, b) MUX_DEF_CHOOSE_2ND(mux_comma a, b)
+#define MUX_DEF_JUDGE(mux_def, macro, a, b) MUX_DEF_JUDGE_COMMA(CONCAT(mux_def, macro), a, b)
+#define MUX_DEF(macro, x, y)  MUX_DEF_JUDGE(MUX_DEF_, macro, x, y)
+
+#define MUX_CON(con, x, y) \
+    if (con) {             \
+        x;                 \
+    }                      \
+    else {                 \
+        y;                 \
+    }                      \
 
 #endif
